33465676f7ba1c7470aedb55942b9edc9595a818
far-id <fcrizkywijaya@gmail.com>
2025-08-20 05:56:50 +0700

    send email

--- api/index.ts
+++ api/index.ts
@@ -5,14 +5,41 @@ const axios = require("axios");
 
 const MAILRY_TOKEN = process.env.MAILRY_TOKEN;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(express.json());
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Route sederhana untuk hit Mailry API
 app.get("/email", async (req, res) => {
-  console.log(MAILRY_TOKEN);
   try {
     const response = await axios.get("https://api.mailry.co/ext/email", {
+      headers: {
+        Authorization: MAILRY_TOKEN,
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        Connection: "keep-alive"
+      },
+    });
+
+    res.json(response.data); // kirim balik ke client
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to fetch email API" });
+  }
+});
+
+app.post("/email", async (req, res) => {
+  console.log(req);
+  const { emailId, to, subject, htmlBody, plainBody, attachments } = req.body;
+  try {
+    const response = await axios.post("https://api.mailry.co/ext/inbox/send", {
+      emailId,
+      to,
+      subject,
+      htmlBody,
+      plainBody,
+      attachments,
+    }, {
       headers: {
         Authorization: MAILRY_TOKEN,
         "Content-Type": "application/json",
